483235c38991ac4efcdb2d1c8bf4ca2acf1762ec
nagarjunev11 <nagarjunev.11@gmail.com>
2023-05-19 13:51:06 -0500

    backend code initial commit

---
+++ payment-be/src/main/java/com/razorpay/demo/RazorpayAppApplication.java
@@ -0,0 +1,13 @@
+package com.razorpay.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RazorpayAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RazorpayAppApplication.class, args);
+	}
+
+}
---
+++ payment-be/src/test/java/com/razorpay/demo/RazorpayAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.razorpay.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RazorpayAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
